29c7304bf1dbef1d4e1ff8ebb42882ff1f39fc4e
Hector Gonzalez <hector.gonzalez@enova.mx>
2016-09-28 15:26:33 -0500

    edit view devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
+ 
+  #santo
+  devise_scope :user do
+    get '/users' => 'devise/registrations#new'
+    get '/users/password' => 'devise/passwords#new'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
