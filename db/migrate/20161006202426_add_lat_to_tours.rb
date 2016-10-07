c543509fed6069748ea06b13b3709309cc0fb166
hectorgool <hectorgool@gmail.com>
2016-10-07 18:04:14 -0500

    delete migrations lat lng

--- db/migrate/20161006202426_add_lat_to_tours.rb
+++
@@ -1,5 +0,0 @@
-class AddLatToTours < ActiveRecord::Migration
-  def change
-    add_column :tours, :lat, :float
-  end
-end
--- db/migrate/20161006202433_add_lng_to_tours.rb
+++
@@ -1,5 +0,0 @@
-class AddLngToTours < ActiveRecord::Migration
-  def change
-    add_column :tours, :lng, :float
-  end
-end
