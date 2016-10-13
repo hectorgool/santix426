bdba6de92a32850f44a0d4ae723114ad14438fb7
Hector Gonzalez <hector.gonzalez@enova.mx>
2016-10-13 08:42:40 -0500

    edit views

--- spec/controllers/admin/application_controller_spec.rb
+++ spec/controllers/admin/application_controller_spec.rb
@@ -2,10 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Admin::ApplicationController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    receive(:current_user).and_return(user).allow(controller).toend
+  end
+
+  context "non-admin users" do
+    it "are not able to access the index action" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to "/"
+      expect(flash[:alert]).to eq "You must be an admin to do that."
     end
   end
 
