beb570c6a5b5e3f696504f981356f3d3c0295b19
Hector Gonzalez <hector.gonzalez@enova.mx>
2016-10-14 12:52:31 -0500

    re-edit footer template

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,14 +1,19 @@
 class ToursController < ApplicationController
 
   def index
-    @tours = Tour.all
+    #@tours = Tour.all
+    @tours = Tour.last(12).reverse   
   end
 
   def show
     @tour = Tour.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-    flash[:alert] = "The tour you were looking for could not be found."
-    redirect_to tours_path  
+    #rescue ActiveRecord::RecordNotFound
+    #flash[:alert] = "The tour you were looking for could not be found."
+    #redirect_to tours_path
+    respond_to do |format|
+      format.html
+      format.json { render json: @tour }
+    end  
   end
 
 end
